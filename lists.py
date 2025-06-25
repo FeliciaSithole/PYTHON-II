45a4e2322fdf5ca2af64e4f360f333c90379dba9
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-06-25 18:56:13 +0200

    correcting the spelling

--- lists.py
+++ lists.py
@@ -1,6 +1,6 @@
 fruits = ["apple", "banana", "cherry"]
 
-fruits.append("kiwi") #add an iteam on the list
+fruits.append("kiwi") #add an item on the list
 print(fruits)
 
 fruits.insert(1, "orange") #add a value to a specific position
@@ -9,7 +9,7 @@ print(fruits)
 fruits.remove("banana") #removing iterm on the list
 print(fruits)
 
-fruits.sort(reverse=True) #sort/arrange the iteams in ascending order for decending order we have to pass an argument with reverse=true
+fruits.sort(reverse=True) #sort/arrange the items in ascending order for decending order we have to pass an argument with reverse=true
 print(fruits)
 
 # fruits[1] = "orange"
