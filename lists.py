230e9ae5abc23cdb01b75c13d7a82800acfd13b8
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-06-25 18:54:50 +0200

    add or remove items in a list

--- lists.py
+++ lists.py
@@ -1,7 +1,17 @@
 fruits = ["apple", "banana", "cherry"]
 
-print(fruits[0])
-
-fruits[1] = "orange"
-
+fruits.append("kiwi") #add an iteam on the list
 print(fruits)
+
+fruits.insert(1, "orange") #add a value to a specific position
+print(fruits)
+
+fruits.remove("banana") #removing iterm on the list
+print(fruits)
+
+fruits.sort(reverse=True) #sort/arrange the iteams in ascending order for decending order we have to pass an argument with reverse=true
+print(fruits)
+
+# fruits[1] = "orange"
+
+# print(fruits)
